c986f1036b16e756e1f50077651174598ad99021
Gnlow <gnlow@pm.me>
2025-09-10 11:42:31 +0900

    add offset

--- src/fit.ts
+++ src/fit.ts
@@ -9,10 +9,10 @@ await pyodide.runPythonAsync(`
     import numpy as np
     from scipy.optimize import curve_fit
 
-    def gompertz(t, a, b, c):
-        return a*np.exp(-b*np.exp(-c*t))
+    def gompertz(t, a, b, c, offset):
+        return a*np.exp(-b*np.exp(-c*(t-offset)))
     
-    def fit(x_data, y_data, initial_guess=[70, 1, 0.1]):
+    def fit(x_data, y_data, initial_guess=[70, 1, 0.1, 0]):
         popt, _ = curve_fit(
             gompertz,
             np.array(x_data),
@@ -29,6 +29,6 @@ async (coords: [number, number][]) => {
     const xData = new Float32Array(coords.map(v => v[0]))
     const yData = new Float32Array(coords.map(v => v[1]))
 
-    return <[number, number, number]>
+    return <[number, number, number, number]>
         [...await pyodide.globals.get("fit")(xData, yData)]
 }
