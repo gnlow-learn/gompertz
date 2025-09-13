5b7fbd90b6315f2294400b0c21e022228cb40e8e
Gnlow <gnlow@pm.me>
2025-09-13 11:26:54 +0900

    add c - not works

--- src/fit.ts
+++ src/fit.ts
@@ -9,15 +9,19 @@ await pyodide.runPythonAsync(`
     import numpy as np
     from scipy.optimize import curve_fit
 
-    def gompertz_cdf(t, a, b, max, offset):
-        return max*(1-np.exp(-a/b*np.expm1((t-offset)*b)))
+    def gompertz_cdf(t, a, b, c, max, offset):
+        return max*(1-c*(t-offset)-np.exp(-a/b*np.expm1((t-offset)*b)))
     
-    def fit(x_data, y_data, initial_guess=[0.05, 0.1, 1, 15]):
+    def fit(x_data, y_data, initial_guess=[0.01, 0.1, 0, 1, 15]):
         popt, _ = curve_fit(
             gompertz_cdf,
             np.array(x_data),
             np.array(y_data),
             p0=initial_guess,
+            bounds=(
+                [0, 0, 0, 0, 0],
+                [np.inf, np.inf, np.inf, np.inf, np.inf]
+            ),
         )
         return popt.tolist()
 
@@ -29,6 +33,6 @@ async (coords: [number, number][]) => {
     const xData = new Float32Array(coords.map(v => v[0]))
     const yData = new Float32Array(coords.map(v => v[1]))
 
-    return <[number, number, number, number]>
+    return <[number, number, number, number, number]>
         [...await pyodide.globals.get("fit")(xData, yData)]
 }
